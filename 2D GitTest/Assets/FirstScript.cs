89340afd5d05ae0242a471989d00bd87bdd552a2
Kentaro Ueda <uedakentarou0825@gmail.com>
2014-03-17 17:57:01 +0900

    ログ表示コードを追加
    FirstScript.cs

--- 2D GitTest/Assets/FirstScript.cs
+++ 2D GitTest/Assets/FirstScript.cs
@@ -3,13 +3,20 @@ using System.Collections;
 
 public class FirstScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+	
+	void Start () 
+    {
 	
 	}
 	
-	// Update is called once per frame
-	void Update () {
 	
+	void Update () 
+    {
+	    
 	}
+
+    void TestLog()
+    {
+        Debug.Log("Git Test");
+    }
 }
